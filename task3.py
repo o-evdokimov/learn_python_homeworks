fe6b6c78c64ae41980e4ab3ba7f039ff5361f509
evdokimov <o.evdokimov@corp.mail.ru>
2019-05-21 14:48:32 +0300

    create new task3

--- task3.py
+++ task3.py
@@ -3,4 +3,14 @@
 
 ### Main ###
 
+school_classes = [
+    {'school_class':'4a','scores':[3,4,5,5,2]}, 
+    {'school_class':'4b','scores':[2,4,4,2,2]},   
+    {'school_class':'4c','scores':[5,4,5,5,3]}, 
+    {'school_class':'4d','scores':[4,4,4,2,4]}, 
+    {'school_class':'4e','scores':[3,4,5,5,2]},   
+]
 
+item = {}
+for item in school_classes:
+    print(item)
