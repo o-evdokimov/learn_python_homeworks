5e0e8c3bf3607b1387bf5b4f93bf1d190bea52b7
evdokimov <o.evdokimov@corp.mail.ru>
2019-05-24 02:43:26 +0300

    4

--- week1/task4.py
+++ week1/task4.py
@@ -4,14 +4,29 @@
 
 def ask_user():
     answer=''
-    while answer != 'Good':
-        answer = input('How are you?')
+    while answer.lower() != 'good':
+        answer = input('How are you? ')
+
+def ask_program():
+    dialog = {
+            'How are you?':"I'm fine",
+            'What are you doing?':"I'm coding"
+    }
+    question=''
+    while question.lower() != 'bye':
+        try:
+            question = input()
+        except KeyError: 
+            print ("Exuse me. I don't understand you")
+        print('{}'.format(dialog[question]))
     
 
 ### Main ###
 
-print('1. ask_user or 2. ask_program')
+print('1. ask_user\n2. ask_program')
 
 select = int(input('Select 1 or 2 ? '))
 
 if select == 1: ask_user()
+elif select == 2: ask_program()
+else: print ('Bye!'), exit(1)
