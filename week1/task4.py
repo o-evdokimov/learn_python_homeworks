e0309be9c83aa91c9b0fb46b05890e2d45850623
evdokimov <o.evdokimov@corp.mail.ru>
2019-05-24 02:17:10 +0300

    4

--- week1/task4.py
+++ week1/task4.py
@@ -2,24 +2,16 @@
 
 # WHILE
 
-
 def ask_user():
-
-    while answer!='Good' {
+    answer=''
+    while answer != 'Good' {
         answer = input('How are you?')
     }
 
-
-
 ### Main ###
 
-print(
-    '''
-    1. ask_user
-    2. ask_program
-    '''
-    )
+print('1. ask_user or 2. ask_program')
 
 select = input('Select 1 or 2 ?')
 
-if select == 1: ask_user
+if select == 1: ask_user()
