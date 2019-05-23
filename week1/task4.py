81afd35f8cded4f26e08e53be4f8fbfc34d0e457
evdokimov <o.evdokimov@corp.mail.ru>
2019-05-24 02:14:06 +0300

    task4

---
+++ week1/task4.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# WHILE
+
+
+def ask_user():
+
+    while answer!='Good' {
+        answer = input('How are you?')
+    }
+
+
+
+### Main ###
+
+print(
+    '''
+    1. ask_user
+    2. ask_program
+    '''
+    )
+
+select = input('Select 1 or 2 ?')
+
+if select == 1: ask_user
