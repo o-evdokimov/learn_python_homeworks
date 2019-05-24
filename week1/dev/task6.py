9484d7afc3e26c60a4a48178a22cba4993082358
evdokimov <o.evdokimov@corp.mail.ru>
2019-05-24 21:27:04 +0300

    6

---
+++ week1/dev/task6.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+#Task6:
+
+# 6.1
+# Напишите функцию get_summ(num_one, num_two), которая принимает на вход два целых числа (int) и складывает их
+
+# 6.2
+# Оба аргумента нужно приводить к целому числу при помощи int() и перехватывать исключение ValueError 
+# если приведение типов не сработало
+
+
+
+def get_sum (i1, i2):
+	try:
+	    sum = int(i1) + int(i2)
+	    print ('sum =', sum)
+	except ValueError:
+		print ('Error: digits are not integer !')
+
+### Main ###
+
+get_sum()
