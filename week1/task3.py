fb14d932606dc7b6c60abb4a04c5e8ecfd28eccf
evdokimov <o.evdokimov@corp.mail.ru>
2019-05-24 19:45:13 +0300

    a

--- week1/task3.py
+++ week1/task3.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
-# FOR
+#$ ./task3.py 
+#Score of class 4a = 3.8
+#Score of class 4b = 2.8
+#Score of class 4c = 4.4
+#Score of class 4d = 3.6
+#Score of class 4e = 3.8
+#------------------------------
+#The medium of scores in school = 3.68
+
 
 ### Main ###
 
@@ -12,18 +20,13 @@ school_classes = [
     {'school_class':'4e','scores':[3,4,5,5,2]},   
 ]
 
-
 item = {}
 _sum = 0
 _len = 0
-_sum_total = 0
-_len_total = 0
 for item in school_classes:
-    _sum = sum(item['scores'])
-    _sum_total = _sum_total + _sum
-    _len = len(item['scores'])
-    _len_total = _len_total + _len
-    print ('Score of class {} = {}'.format(item['school_class'],_sum/_len))
+    _sum_of_class = sum(item['scores'])
+    _sum_total += _sum_of_class
+    _number_of_scores = len(item['scores'])
+    _number_total += _number_of_scores
 
-print ('-'*30)
-print ('{}{}'.format('The medium of scores in school = ',_sum_total/_len_total))
+print ('{}{}{}{}'.format('The medium of scores in school=',_sum_total / _number_total))
