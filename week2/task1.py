92923e9e9edab8630a22697bf12512cd6f27870d
evdokimov <o.evdokimov@corp.mail.ru>
2019-05-30 01:12:17 +0300

    task1

--- week2/task1.py
+++ week2/task1.py
@@ -1,3 +1,12 @@
 #!/usr/local/bin/python3
 
+from datetime import date, datetime, timedelta
+import dateutil.relativedelta
+
+date_now = datetime.now()
+date_yesterday = (date_now - timedelta(days=1)).strftime('%Y-%m-%d')
+date_month_ago = (date_now + dateutil.relativedelta.relativedelta(months=-1)).strftime('%Y-%m-%d')
+date_now = datetime.today().strftime('%Y-%m-%d')
+
+print ('\nToday: {}\nYesterday: {}\nMonth ago: {}\n'.format(date_now, date_yesterday, date_month_ago))
 
