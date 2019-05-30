5faddb01a96b6910f34dc41f333ff2d0b328403d
evdokimov <o.evdokimov@corp.mail.ru>
2019-05-31 01:19:50 +0300

    t3

---
+++ week2/task3.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+
+# Week2 Task3
+#
+# Создайте список словарей:
+#        [
+#        {'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
+#        {'name': 'Вася', 'age': 8, 'job': 'Programmer'}, 
+#        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+#    ]
+# Запишите содержимое списка словарей в файл в формате csv
+#
+# Output:
+#
+#
