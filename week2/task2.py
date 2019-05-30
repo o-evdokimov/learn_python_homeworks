0a10c7de7a74773beac93c3bb792223e45c69d0b
evdokimov <o.evdokimov@corp.mail.ru>
2019-05-31 01:16:01 +0300

    task2

--- week2/task2.py
+++ week2/task2.py
@@ -9,10 +9,21 @@
 #
 # Output:
 #
+# len_of_string: 1509
+# number_of_words: 159
+# referat2 is created
+#
 
 referat_str=''
 
 with open('referat.txt', 'r', encoding = 'utf-8') as referat:
     referat_str = referat.read()
 
-print ('len_of_string: {}'.format(len(referat_str)))
+print ('\nlen_of_string: {}'.format(len(referat_str)))
+print ('number_of_words: {}'.format(len(referat_str.split(' '))))
+
+referat_str = referat_str.replace ('.','!')
+
+with open('referat2.txt', 'w', encoding = 'utf-8') as referat2:
+    referat2.write(referat_str)
+    print ('referat2 is created \n')
