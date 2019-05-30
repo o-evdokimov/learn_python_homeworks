62f33a8a96d01be546e4275f89e3cb9675ffb88e
evdokimov <o.evdokimov@corp.mail.ru>
2019-05-31 01:26:46 +0300

    t3

--- week2/task3.py
+++ week2/task3.py
@@ -13,3 +13,19 @@
 # Output:
 #
 #
+
+import csv
+
+list_of_dicts = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'}, 
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+]
+
+
+with open ('csv_file.csv', 'w', encoding = 'utf-8') as csvfile:
+    tops = ['name ','age','job']
+    reader = csv.DictReader (csvfile, tops, delimiter=',')
+    for row in reader:
+        csvfile.write(row)
+        
